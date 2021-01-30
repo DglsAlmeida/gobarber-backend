af3edb17607836a05eb6400f7fb5233cae61cd3e
Douglas Almeida <douglas.alu.lmb@gmail.com>
2021-01-30 12:46:33 -0300

    finished tests

---
+++ src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -0,0 +1,18 @@
+import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
+import { Router } from 'express';
+import AppointmentsController from '../controllers/AppointmentsController';
+
+const appointmentsRouter = Router();
+
+appointmentsRouter.use(ensureAuthenticated);
+
+// appointmentsRouter.get('/', async (request, response) => {
+//   const appointments = await appointmentsRepository.find();
+
+//   return response.json(appointments);
+// });
+const appointmentsController = new AppointmentsController();
+
+appointmentsRouter.post('/', appointmentsController.create);
+
+export default appointmentsRouter;
